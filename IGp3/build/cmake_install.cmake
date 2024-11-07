096a1acbca1825e76c4628434000c1623eae56a4
isamorro <isabelmorro@hotmail.com>
2024-11-07 12:28:06 +0100

    ya ejecuta la p3

--- IGp3/build/cmake_install.cmake
+++ IGp3/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/isabel/Escritorio/IG/IGp2/builds/linux
+# Install script for directory: /home/isabel/Escritorio/IG/IGp3/builds/linux
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
